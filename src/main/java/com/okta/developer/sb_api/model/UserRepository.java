c8b910df4bfc33bec6291abaf60629bdbd53b384
William ☕️ <113069637+williamx0@users.noreply.github.com>
2024-07-03 15:49:43 +0100

    update the controller

--- src/main/java/com/okta/developer/sb_api/model/UserRepository.java
+++ src/main/java/com/okta/developer/sb_api/model/UserRepository.java
@@ -2,6 +2,6 @@ package com.okta.developer.sb_api.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String>{
+public interface UserRepository extends JpaRepository<User, String> {
 
 }
