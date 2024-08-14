fd66914d2083f3e95b351eef65e184d82f2b6e27
William ☕️ <113069637+williamx0@users.noreply.github.com>
2024-08-14 13:37:56 +0100

    -update group controller

--- src/main/java/com/okta/developer/sb_api/config/CookieCsrfFilter.java
+++ src/main/java/com/okta/developer/sb_api/config/CookieCsrfFilter.java
@@ -7,7 +7,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
- import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
